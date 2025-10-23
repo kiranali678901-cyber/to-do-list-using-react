eb17d4dbb5517848d19e79ca01ae244c9b2d0539
kiranali678901-cyber <kiranali678901@gmail.com>
2025-10-23 20:48:16 +0300

    adding animation

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,18 @@ function App() {
   };
 
   const taskList = () => {
+    if(task==='' || details===''){
+      alert('please fill details');
+      return
+    }
     const newList = { task, details };
     setList([...list, newList]);
   };
-  const deleteTask = (idx)=>{
-   const updateList = [...list]
-   updateList.splice(idx,1)
-   setList(updateList)
-
-  }
+  const deleteTask = (idx) => {
+    const updateList = [...list];
+    updateList.splice(idx, 1);
+    setList(updateList);
+  };
   return (
     <>
       <div className="px-5 py-5 flex  gap-10">
@@ -30,6 +33,7 @@ function App() {
           <input
             type="text"
             placeholder="Add your Task"
+            required
             value={task}
             onChange={(e) => {
               setTask(e.target.value);
@@ -38,6 +42,7 @@ function App() {
           ></input>
           <textarea
             placeholder="Add Task Details"
+            required
             value={details}
             onChange={(e) => {
               setDetails(e.target.value);
@@ -46,7 +51,7 @@ function App() {
           ></textarea>
           <button
             onClick={taskList}
-            className="bg-black text-white rounded px-3 py-2"
+            className="bg-black text-white rounded px-3 py-2 hover:scale-95"
           >
             Submit
           </button>
@@ -55,7 +60,7 @@ function App() {
           {list.map(function (elem, idx) {
             return (
               <div
-                key={idx} 
+                key={idx}
                 className="h-60 w-50 border rounded px-7 py-5 flex flex-col justify-between"
                 style={{
                   background: `url(https://pic.mksucai.com/00/45/32/ee6716cf53affa57.webp)`,
@@ -67,9 +72,14 @@ function App() {
                   <h2 className="font-black">{elem.task}</h2>
                   <p>{elem.details}</p>
                 </div>
-                <button onClick={()=>{
-                  deleteTask(idx)
-                }}  className="text-end bg-amber-400 p-1 flex align-center justify-center rounded">Delete</button>
+                <button
+                  onClick={() => {
+                    deleteTask(idx);
+                  }}
+                  className="text-end bg-amber-400 p-1 flex align-center justify-center rounded hover:scale-90"
+                >
+                  Delete
+                </button>
               </div>
             );
           })}
